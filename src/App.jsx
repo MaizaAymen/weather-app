2e44c7ab3bb4f40063bb08b81ce63701fb555d10
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-02 23:04:04 +0100

    `Update README.md to reflect React + Vite setup`

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+const api={
+  key:"87bdf686e84f5a6de051ae507ec00cdb",
+  base:"https://api.openweathermap.org/data/2.5/"
+
+};
+
+function App() {
+  const [search, setsearch] = useState("")
+  const serchPres=()=>{
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+    .then((res)=>res.json())
+    .then((result)=>{
+      console.log(result)
+    })
+
+  }
+
+  return (
+    <>
+     <div className="App">
+      <header className="App-header"><h1>Weather App</h1></header>
+      <div><input type="text" onChange={(e)=>setsearch(e.target.value)}/>
+      <button onClick={serchPres}>aymen</button>
+      </div>
+      
+     </div>
+    </>
+  )
+}
+
+export default App
